dd233fe7bf974e0b100075139960729f6f09b274
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-08-23 13:43:25 -0700

    Add files via upload

---
+++ Practice01MathIterative.java
@@ -0,0 +1,14 @@
+public class Practice01MathIterative implements Practice01Math {
+
+		@Override
+	public int fib (int n){
+		return 1;
+
+	}
+
+	@Override
+	public int fact (int n) {
+		return 1;
+	}
+
+}
---
+++ Practice01MathRecursive.java
@@ -0,0 +1,13 @@
+public class Practice01MathRecursive implements Practice01Math {
+
+	@Override
+	public int fib (int n){
+		return 1;
+
+	}
+
+	@Override
+	public int fact (int n) {
+		return 1;
+	}
+}
