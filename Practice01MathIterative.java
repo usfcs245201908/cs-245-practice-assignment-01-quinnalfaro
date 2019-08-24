f8e8e5e1a3cf7476a320a4a796c9cbbabf100030
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-08-24 10:20:28 -0700

    Add files via upload

--- Practice01MathIterative.java
+++ Practice01MathIterative.java
@@ -1,14 +1,27 @@
 public class Practice01MathIterative implements Practice01Math {
 
-		@Override
+	@Override
 	public int fib (int n){
+		if (n < 0){
+			throw new IllegalArgumentException();
+		} 
+		while (n < 1) {
+			return 1;
+		}
 		return 1;
-
 	}
+
 
 	@Override
 	public int fact (int n) {
-		return 1;
+		int returnVal = 1;
+		if (n < 0){
+			throw new IllegalArgumentException();
+		} 
+		while (n > 1){
+			returnVal = returnVal * n;
+			 n = n - 1;
+		}
+		return returnVal;
 	}
-
 }
--- Practice01MathRecursive.java
+++ Practice01MathRecursive.java
@@ -2,12 +2,22 @@ public class Practice01MathRecursive implements Practice01Math {
 
 	@Override
 	public int fib (int n){
-		return 1;
-
+		if (n < 0){
+			throw new IllegalArgumentException();
+		} else if (n <= 1) {
+			return n;
+		}
+		return fib(n - 1) + fib(n - 2);
 	}
+
 
 	@Override
 	public int fact (int n) {
-		return 1;
+		if (n < 0){
+			throw new IllegalArgumentException();
+		} else if (n <= 1) {
+			return n;
+		}
+		return fib(n - 1) + fib(n - 2);
 	}
 }
